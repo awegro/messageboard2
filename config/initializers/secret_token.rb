0bbdd41ee519bc487ca7ecdf552ee55ebb8773b8
Andrew Elliott <contact@awegroup.com>
2013-07-27 11:16:59 -0700

    Initial Rails App with Home Controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Messageboard2::Application.config.secret_token = '8150a2c49ea8a2792d6c3335832d4fdd5845ebabab817c624dd2120f4b1bba9f23e6504269018f147af8a0c85cb51d8d4687592fec85db8bb851c4c6673278f7'
